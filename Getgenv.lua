abbd33b45bb90f18f2f97f0fb7a1c44a9f3aa4eb
Chattheobuo <nguyenthisever@gmail.com>
2024-08-25 20:14:12 +0700

    Create getkey.lua

---
+++ Getgenv.lua
@@ -0,0 +1,23 @@
+getgenv().Version == "NoKey"
+else
+getgenv().Version == "GetKey"
+end
+-- Name Loading!! 
+local Sea = "Sea Event"
+  getgenv().NameSea = {
+   Name = "Auto Crafts Shark Anchor"
+   DisName = "I don't know if it works!"
+   Name1 = "Auto Find leviathan"
+   DisName1 = "Need 5 Players"
+   Name2 = "Auto Attack Leviathan"
+   Name3 = "Auto Doge Skill Terro Shark"
+   Name4 = "Auto Doge Skill Sea Beast"
+   Name5 = "Auto Doge Rough Sea" or BTP then
+}
+   
+--[[getgenv().NameHub1 = "X-Sea Revamped"
+else
+getgenv().NameHub = "" or NameHub1 then
+end --]]
+-- ThatBuonCuoi... vi anh da danh mat emm
+   
