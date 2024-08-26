7728d41893e4704a12bc3725b0125bdb29ff6fba
Chattheobuo <nguyenthisever@gmail.com>
2024-08-26 20:33:29 +0700

    Update Getgenv.lua

--- Getgenv.lua
+++ Getgenv.lua
@@ -1,20 +1,16 @@
-getgenv().Version == "NoKey"
-else
-getgenv().Version == "GetKey"
-end
 -- Name Loading!! 
-if getgenv().Version = "GetKey" then
+if getgenv().Version == "GetKey" then
 local Sea = "Sea Event"
-  getgenv().NameSea = {
-   Name = "Auto Crafts Shark Anchor"
-   DisName = "I don't know if it works!"
-   Name1 = "Auto Find leviathan"
-   DisName1 = "Need 5 Players"
-   Name2 = "Auto Attack Leviathan"
-   Name3 = "Auto Doge Skill Terro Shark"
-   Name4 = "Auto Doge Skill Sea Beast"
-   Name5 = "Auto Doge Rough Sea" or BTP then
-end}
+  local Page21 = {
+   Page1:Toggle = "Auto Crafts Shark Anchor"
+--   DisName = "I don't know if it works!"
+   Page2:Toggle = "Auto Find leviathan"
+--   DisName1 = "Need 5 Players"
+   Page2:Toggle = "Auto Attack Leviathan"
+   Page1:Toggle = "Auto Doge Skill Terro Shark"
+   Page1:Toggle = "Auto Doge Skill Sea Beast"
+   Page2:Toggle = "Auto Doge Rough Sea"
+ end}
    
 --[[getgenv().NameHub1 = "X-Sea Revamped"
 else
