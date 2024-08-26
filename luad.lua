9876b17c5904d286649089aeca5ccbb3d94873bc
Chattheobuo <nguyenthisever@gmail.com>
2024-08-26 12:20:24 +0700

    Update luad.lua

--- luad.lua
+++ luad.lua
@@ -1,13 +1,10 @@
 getgenv().Script == "Main"
-else
+elseif
 getgenv().Script == "AutoChest"
 end
-
 if getgenv().Script == "Main" then
-getgenv().JoinTeam = "Pirates"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Chattheobuo/getgenv/main/XSeaLow", true))()
 end
-
 if getgenv().Script == "AutoChest" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Chattheobuo/getgenv/main/mainloader.lua", true))()
 end
