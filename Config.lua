41b84b7223af26a2ae0ad5d879cc7598ce11a471
Chattheobuo <nguyenthisever@gmail.com>
2024-09-06 16:08:29 +0700

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,20 @@
+function FastAttack = ["FireCountdown"] 
+or ["00.1"] 
+local Config = {
+   ["Double Quest"] = false
+   ["Triple Quest"] = true
+  if FastAttack = {
+   ["Attack No CD Y"] = "0.1"
+   ["Attack Mob"] = false
+   MethodFarm = {
+   ["Upper"]
+       ["ConCac"]
+         ["ZicZac"]
+         ["UtralTic"] 
+             ["Hop Severs Low Players"]
+        }
+    }
+end}
+    
+       
+   
