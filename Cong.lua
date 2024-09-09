826e48fbe7a4d1376c2e5beb69d1e4c1e2569a41
Chattheobuo <nguyenthisever@gmail.com>
2024-09-09 15:31:55 +0700

    Create Cong.lua

---
+++ Cong.lua
@@ -0,0 +1,32 @@
+-- function Get Items and Farm
+-- ?
+getgenv().Config = {
+    ["Auto Farm Level"] = false,
+    ["Auto Farm Bone"] = false,
+    ["Auto Pull Level"] = Value
+    ["Value"] = Value
+    Quest = {
+        ["Double Quest"] = true,
+        ["Triple Quest"] = false,
+    FastAttack = {
+        ["Fast Attack"] = true,
+        ["Fast Delay"] = "0.1"
+        ["Attack No CD"] = true,
+        ["BringMob"] = true
+        ["Select Bring Mob"] = "450"
+    SeaEvent = {
+        ["Auto Sail Boat To Sea"] = false,
+        ["Auto Kill Terro Shark"] = false
+        ["Auto Kill Shark + Piranha"] = false,
+        ["Kill Fish Crew"] = false
+        ["Doge Skill"] = true
+        ["Next Out Rough Sea"] true,
+        ["Auto Kill Leviathan"] = false,
+        ["Find Leviathan"] = false
+              ["Spinning PosY"] 
+              ["Random Cframe When Farm Level"] = true,
+        }
+    },
+}
+        
+    
